4b21e3a296ee3ddc8edfebcc4c6d652da40b87d9
Miad Jalili <jalilimiad@gmail.com>
2022-06-14 21:30:55 +0430

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by miadjalili on 6/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGreen
+    }
+
+
+}
+
